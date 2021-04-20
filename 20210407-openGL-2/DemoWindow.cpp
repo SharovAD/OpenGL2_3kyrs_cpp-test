09f61bf5a09853230b52d0d7a6e9892295fb6c97
kompU510 <kompU510@192.168.56.10>
2021-04-20 19:03:20 +0500

    Проект скопирован с предыдущего задания

---
+++ 20210407-openGL-2/DemoWindow.cpp
@@ -0,0 +1,117 @@
+/*
+ * DemoWindow.cpp
+ *
+ *  Created on: 19 апр. 2021 г.
+ *      Author: kompU510
+ */
+
+#include "DemoWindow.h"
+
+DemoWindow::DemoWindow(int width, int height)
+: Window(width, height)
+{
+	set_title("OpenGL Demo Window");
+}
+
+void DemoWindow::setup_gl()
+{
+	glEnable(GL_DEPTH_TEST);// решение неправильного отображения куба
+
+
+	glClearColor(0.2f, 0.4f, 0.7f, 1.0f);
+
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(
+			45.0,								// Угол обзора по вертикали
+			double(width()) / double(height()),	// Соотношение сторон
+			0.1,								// Расстояние до ближней ПО
+			20.0);								// Расстояние до дальней ПО
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void DemoWindow::render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Очистка
+
+	glLoadIdentity(); // MV = единичная матрица
+
+	gluLookAt(	3.0, 4.0, 2.0,		// Координаты камеры
+				0.0, 0.0, 0.0,		// Координаты центра
+				0.0, 0.0, 1.0);		// Направление вверх,	MV = C
+
+	glRotated(_cube_angle, 0.0, 0.0, 1.0);
+
+	glBegin(GL_QUADS);
+
+	glNormal3d(  0.,  0.,  -1.);
+	glColor3d(1.0, 0.0, 0.0);		// Нижняя грань, красная
+	glVertex3d(  1.,  1., -1.);
+	glVertex3d( -1.,  1., -1.);
+	glVertex3d( -1., -1., -1.);
+	glVertex3d(  1., -1., -1.);
+
+	glNormal3d(  0.,  0., 1.);
+	glColor3d(0.0, 1.0, 1.0);		// Верхняя грань, бирюзовая
+	glVertex3d( -1.,  1.,  1.);
+	glVertex3d(  1.,  1.,  1.);
+	glVertex3d(  1., -1.,  1.);
+	glVertex3d( -1., -1.,  1.);
+
+	glNormal3d(  1.,  0.,  0.);
+	glColor3d(0.0, 0.0, 1.0);		// Боковая грань1, синяя
+		glVertex3d(1.0, -1.0,  1.0);
+		glVertex3d( 1.0, 1.0,  1.0);
+		glVertex3d( 1.0,  1.0,  -1.0);
+		glVertex3d(1.0,  -1.0,  -1.0);
+
+		glNormal3d( -1.,  0.,  0.);
+		glColor3d(1.0, 0.0, 1.0);		// Боковая грань2, розовая
+				glVertex3d(-1.0, 1.0,  1.0);
+				glVertex3d( -1.0, -1.0,  1.0);
+				glVertex3d( -1.0,  -1.0,  -1.0);
+				glVertex3d(-1.0,  1.0,  -1.0);
+
+				glNormal3d(  0.,  1.,  0.);
+		glColor3d(1.0, 1.0, 1.0);		// Боковая грань3, белая
+				glVertex3d(-1.0, 1.0,  1.0);
+				glVertex3d( 1.0, 1.0,  1.0);
+				glVertex3d( 1.0,  1.0,  -1.0);
+				glVertex3d(-1.0,  1.0,  -1.0);
+
+				glNormal3d(  0., -1.,  0.);
+				glColor3d(0.0, 0.0, 0.0);		// Боковая грань4, черная
+						glVertex3d(1.0, -1.0,  1.0);
+						glVertex3d( -1.0, -1.0,  1.0);
+						glVertex3d( -1.0,  -1.0,  -1.0);
+						glVertex3d(1.0,  -1.0,  -1.0);
+
+
+
+	/* TODO: задание для самостоятельной работы
+		 * - Добавить оставшиеся грани куба
+		 * - Выяснить, почему куб в текущем виде отображается неправильно
+		 * - Исправить программу, чтобы куб начал правильно отображаться
+		 * - Разобраться с основными преобразованиями:
+		 * 		~ параллельный перенос;
+		 * 		~ поворот;
+		 * 		~ масштабирование.
+		 * - Разобраться с функциями glPushMatrix() и glPopMatrix()
+		 * - Построить из кубиков пирамидку:
+		 *          +-+
+		 *          |X|
+		 *        +-+ +-+
+		 *        |X| |X|
+		 *      +-+ +-+ +-+
+		 *      |X| |X| |X|
+		 *      +-+ +-+ +-+
+		 */
+
+	glEnd();
+}
+
+void DemoWindow::update()
+{
+	_cube_angle += 0.1;
+	if (_cube_angle >= 360.0)
+		_cube_angle -= 360.0;
+}
